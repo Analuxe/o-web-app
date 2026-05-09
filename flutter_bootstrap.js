5fa3b9b613db8b4b1653ca0d90e235c64913575b
Analuxe <Analuxe@users.noreply.github.com>
2026-05-09 23:03:33 +0000

    deploy: 63429a8f9e2235b10e09ead204dc8a71bcd77a61

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "430926396"
+    serviceWorkerVersion: "3779742712"
   }
 });
