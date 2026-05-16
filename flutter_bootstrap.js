a3c312d02b3d70b388a6047622706ff99f4a3009
Analuxe <Analuxe@users.noreply.github.com>
2026-05-16 13:04:13 +0000

    deploy: 65c9c041844a2bcb7c3b4406d810801f8c4b6012

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"73dc1ccd62aec198da4aefde1dae20b1167b13
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2663903055" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1441651352" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
